8eacaf612fe08dee2f2e48189d31fb78dca45df4
Matija997 <matijamarinkovic@gmail.com>
2024-08-15 16:19:02 +0200

    Makeking backup for data base every time applicationi is started

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 import main_menu_window
+import database as db
+import os
+import shutil
 
-
+conn = db.get_connection()
+files = os.listdir('backups')
+version = len(files)
+backup_name = f"backups/data_{version}.0.db"
+shutil.copy('data.db', backup_name)
+conn.close()
 main_menu_window.create_main_menu()
