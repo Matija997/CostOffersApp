5aa4b1d90d3777a82ddd0082e4db5e2b7fa44250
Matija997 <matijamarinkovic@gmail.com>
2024-10-07 15:59:32 +0200

    Changed type of saving backups

--- app.py
+++ app.py
@@ -4,9 +4,20 @@ import os
 import shutil
 
 conn = db.get_connection()
-files = os.listdir('backups')
-version = len(files)
-backup_name = f"backups/data_{version}.0.db"
-shutil.copy('data.db', backup_name)
+
+backup_files = sorted(
+    [f for f in os.listdir('backups')
+     if f.startswith('data_') and f.endswith('.db')],
+    key=lambda x: int(x.split('_')[1].split('.')[0]), reverse=True
+)
+
+for file in backup_files:
+    version = int(file.split('_')[1].split('.')[0])
+    new_version = version + 1
+    new_name = f"backups/data_{new_version}.db"
+    shutil.move(f'backups/{file}', new_name)
+
+new_backup = "backups/data_1.db"
+shutil.copy('data.db', new_backup)
 conn.close()
 create_main_menu()
